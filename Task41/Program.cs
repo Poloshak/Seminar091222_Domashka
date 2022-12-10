86e37fb24e98cf8db45b189fd7e902d54f37a97e
Poloshak <e.l.fomenko@yandex.ru>
2022-12-10 13:17:23 +0300

    tasks 41, 43

---
+++ Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int NumsOverZero (string[] arr)
+{
+    int count = 0;
+    for(int i = 0; i<arr.Length; i++)
+    {
+        if (int.Parse(arr[i])>0) count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите числа через пробел: ");
+string[] M = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine($"Введено {NumsOverZero(M)} чисел больше 0");
---
+++ Task43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double KoordinatyTochkiPeresecheniya (string[] koord)
+{
+    double b1 = int.Parse(koord[0]);
+    double k1 = int.Parse(koord[1]);
+    double b2 = int.Parse(koord[2]);
+    double k2 = int.Parse(koord[3]);
+    double x = (b2-b1)/(k1-k2);
+    double y = k1*x+b1;
+    Console.WriteLine($"Координаты точки пересечения ({x}; {y})");
+    return x;
+    return y;
+}
+Console.Clear();
+Console.WriteLine("Введите b1, k1, b2 и k2 через пробел: ");
+string[] K = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+KoordinatyTochkiPeresecheniya(K);
+
